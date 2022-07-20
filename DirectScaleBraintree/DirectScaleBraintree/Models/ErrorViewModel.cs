6ff5b44c71a763ee73b2eb06f17c094d02a6245e
Russ K <russell@directscale.com>
2022-07-20 17:36:27 -0600

    Initial Project Creation

---
+++ DirectScaleBraintree/DirectScaleBraintree/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DirectScaleBraintree.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
